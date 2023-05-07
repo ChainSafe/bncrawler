f3085f4b6cc7b0e952a2053ec8b09f2c3cb65148
chad <chad.nehemiah94@gmail.com>
2023-05-07 12:55:03 -0500

    feat: added cli + init options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import yargs from 'yargs';
 import figlet from 'figlet';
 import { render } from './charts/screen.js';
+import { Crawler } from './crawler/crawler.js';
 
 // #TODO: Show this and node crawler spinner then render charts
+// const crawler =  await Crawler.init({
+//   //db: 'sequalize',
+//   ///discv5Opts:
+// });
+
 // figlet('Beacon Node Crawler', (err, data) => {
 //   if (err) {
 //     console.error(`Failed to generate header: ${err}`);
@@ -11,5 +17,6 @@ import { render } from './charts/screen.js';
 
 //   console.log(data);
 // });
+
 
 render()
